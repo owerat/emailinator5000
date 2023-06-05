b7032b4ab657ac202f3c267ed1fd2f4856b2fa0a
owerat <135650175+owerat@users.noreply.github.com>
2023-06-05 17:10:01 -0400

    fixed issue with results.
    fixed issue with results. now using a template

--- emailinator5000.py
+++ emailinator5000.py
@@ -3,7 +3,7 @@ import re
 
 app = Flask(__name__)
 
-# route for homepage
+# Define the route for the homepage
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -11,17 +11,24 @@ def index():
         email_addresses = extract_email_addresses(text)
         unique_emails = remove_duplicates(email_addresses)
         save_emails(unique_emails)
-        return render_template('result.html', email_list=unique_emails)
+        return render_template('result.html', emails_line_by_line='\n'.join(unique_emails), emails_single_line=';'.join(unique_emails))
     return render_template('index.html')
 
-# extract email addresses 
+# Function to extract email addresses from the text
 def extract_email_addresses(text):
     pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     return re.findall(pattern, text)
 
-# remove dupes
+# Function to remove duplicate email addresses
 def remove_duplicates(email_list):
     return list(set(email_list))
 
+# Function to save the email addresses to a file
+def save_emails(email_list):
+    with open('emails.txt', 'w') as file:
+        for email in email_list:
+            file.write(email + '\n')
+
+# Run the Flask application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
