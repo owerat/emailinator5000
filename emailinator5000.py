72298fedc4dc4bad807e54d2698f92e5e3c88a41
owerat <135650175+owerat@users.noreply.github.com>
2023-06-05 15:39:30 -0400

    Update emailinator5000.py
    removed save function as it is unused with the webapp. added dedupe.

--- emailinator5000.py
+++ emailinator5000.py
@@ -3,23 +3,25 @@ import re
 
 app = Flask(__name__)
 
+# route for homepage
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    emails_line_by_line = ""
-    emails_single_line = ""
     if request.method == 'POST':
         text = request.form['text']
-        email_pattern = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
-        emails = re.findall(email_pattern, text)
-        
-        with open('emails.txt', 'w') as f:
-            for email in emails:
-                f.write(email + "\n")
+        email_addresses = extract_email_addresses(text)
+        unique_emails = remove_duplicates(email_addresses)
+        save_emails(unique_emails)
+        return render_template('result.html', email_list=unique_emails)
+    return render_template('index.html')
 
-        emails_line_by_line = "\n".join(emails)
-        emails_single_line = "; ".join(emails)
-        
-    return render_template('index.html', emails_line_by_line=emails_line_by_line, emails_single_line=emails_single_line)
+# extract email addresses 
+def extract_email_addresses(text):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.findall(pattern, text)
+
+# remove dupes
+def remove_duplicates(email_list):
+    return list(set(email_list))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
